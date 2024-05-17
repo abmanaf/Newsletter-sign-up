ffadee3ae6bdb72410055e87d517722d3abde87b
Alibaba <abdulmanafaliu414@gmail.com>
2024-05-17 01:53:19 +0000

    minor changes

---
+++ src/Modal.jsx
@@ -0,0 +1,26 @@
+// Modal.js
+import React from 'react';
+import './Modal.css';
+
+function Modal({ show, onClose, email }) {
+  if (!show) {
+    return null;
+  }
+
+  return (
+    <div className="modal-overlay">
+      <div className="modal-content">
+        <div className='img-thanks' style={{ display: 'flex', alignItems: 'center', fontSize: '40em' }}>
+            <img src="src/assets/images/icon-list.svg" alt="" />
+        </div>
+        <div style={{textAlign: 'start'}}>
+            <h2>Thanks for subscribing!</h2>
+            <p>A confirmation email has been sent to <br /> <strong>{email}</strong>  Please open it <br /> and click the button inside to confirm your <br /> subscription.</p>
+        </div>
+        <button onClick={onClose} className="modal-button">Dismiss</button>
+      </div>
+    </div>
+  );
+}
+
+export default Modal;
