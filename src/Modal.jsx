5db835cc25a97020946519b3acba509ab3e03cf5
Alibaba <abdulmanafaliu414@gmail.com>
2024-06-27 19:55:56 +0000

    minor changes

--- src/Modal.jsx
+++ src/Modal.jsx
@@ -9,12 +9,12 @@ function Modal({ show, onClose, email }) {
   return (
     <div className="modal-overlay">
       <div className="modal-content">
-        <div className='img-thanks' style={{ display: 'flex', alignItems: 'center', fontSize: '40em' }}>
-            <img src="src/assets/images/icon-list.svg" alt="icon-list" />
+        <div className='img-thanks' style={{ display: 'flex', alignItems: 'center' }}>
+            <img src="../assets/images/icon-list.svg" alt="icon-list" />
         </div>
         <div style={{textAlign: 'start'}}>
-            <h2>Thanks for subscribing!</h2>
-            <p>A confirmation email has been sent to <br /> <strong>{email}</strong>  Please open it <br /> and click the button inside to confirm your <br /> subscription.</p>
+            <h1 style={{fontSize: '3em', marginBottom: '0.3em'}}>Thanks for subscribing!</h1>
+            <p style={{fontSize: '14px'}}>A confirmation email has been sent to  <strong>{email}</strong>  Please open it  and click the button inside to confirm your subscription.</p>
         </div>
         <button onClick={onClose} className="modal-button">Dismiss Message</button>
       </div>
