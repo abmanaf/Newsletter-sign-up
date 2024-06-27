a43fe613acfe0334ad7ee8c9a8194a6369d3b0cf
Alibaba <abdulmanafaliu414@gmail.com>
2024-06-27 20:44:28 +0000

    minor changes

--- src/Modal.jsx
+++ src/Modal.jsx
@@ -18,7 +18,18 @@ function Modal({ show, onClose, email }) {
         </div>
         <button onClick={onClose} className="modal-button">Dismiss Message</button>
       </div>
+       {/*  <div className="modal-content">
+        <div className='img-thanks' style={{ display: 'flex', alignItems: 'center' }}>
+            <img src="../assets/images/icon-list.svg" alt="icon-list" />
+        </div>
+        <div style={{textAlign: 'start'}}>
+            <h1 style={{fontSize: '2em', marginBottom: '0.3em'}}>Thanks for subscribing!</h1>
+            <p style={{fontSize: '14px'}}>A confirmation email has been sent to  <strong>{email}</strong>  Please open it  and click the button inside to confirm your subscription.</p>
+        </div>
+        <button onClick={onClose} className="modal-button">Dismiss Message</button>
+      </div>*/}
     </div>
+   
   );
 }
 
