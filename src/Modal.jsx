1b27fcafef0973fc9349b67501f2f5027aca1672
Alibaba <abdulmanafaliu414@gmail.com>
2024-06-27 20:36:30 +0000

    minor changes

--- src/Modal.jsx
+++ src/Modal.jsx
@@ -13,7 +13,7 @@ function Modal({ show, onClose, email }) {
             <img src="../assets/images/icon-list.svg" alt="icon-list" />
         </div>
         <div style={{textAlign: 'start'}}>
-            <h1 style={{ marginBottom: '0.3em'}}>Thanks for subscribing!</h1>
+            <h1 style={{fontSize: '2em', marginBottom: '0.3em'}}>Thanks for subscribing!</h1>
             <p style={{fontSize: '14px'}}>A confirmation email has been sent to  <strong>{email}</strong>  Please open it  and click the button inside to confirm your subscription.</p>
         </div>
         <button onClick={onClose} className="modal-button">Dismiss Message</button>
